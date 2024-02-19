7478dccc231d7a8b1f5675de4e07550c3e5833d3
Ömer Faruk Yanık <omerfarukyanik212313@gmail.com>
2024-02-19 16:48:17 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+try
+{
+    Console.WriteLine("Bir sayı giriniz:");
+    int sayi = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Girmiş olduğunuz sayı: " + sayi);
+}
+catch (Exception ex)
+{
+    Console.WriteLine("Hata: " + ex.Message.ToString());
+}
+
+try
+{
+    int a = int.Parse("-20000000000");
+}
+catch (ArgumentNullException ex)
+{
+
+    Console.WriteLine("Boş değer girdiniz");
+    Console.WriteLine(ex);
+}
+catch (FormatException ex)
+{
+    Console.WriteLine("Veri tipi uygun değil.");
+    Console.WriteLine(ex);
+}
+catch (OverflowException ex)
+{
+    Console.WriteLine("Çok küçük ya da çok nüyük bir değer girdiniz.");
+    Console.WriteLine(ex);
+}
+finally
+{
+    Console.WriteLine("İşlem başarıyla tamamlandı.");
+}
